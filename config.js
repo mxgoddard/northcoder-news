b1b65f418dbf38b31b73694944a2da9efc2c9f21
Max Goddard <maxolivergoddard@gmail.com>
2018-10-14 17:35:07 +0100

    Updated .gitignore

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let env = process.env.NODE_ENV || 'development';
+let env = process.env.NODE_ENV || 'production';
 
 const config = {
     development: {
@@ -6,6 +6,9 @@ const config = {
     },
     test: {
         DB_URL: 'mongodb://localhost:27017/ncreddit'
+    },
+    production: {
+        DB_URL: 'mongodb://mxgoddard:Talisman1@ds131753.mlab.com:31753/db_ncreddit'
     }
 }
 
