1c9022968bea103e3031ebf8fabdd0dc4565f210
Max Goddard <maxolivergoddard@gmail.com>
2018-10-14 17:40:49 +0100

    Updated listen.js to work with heroku

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const app = require('./app.js');
 const port = 9090;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(process.env.PORT || port)
 
 module.exports = { app };
